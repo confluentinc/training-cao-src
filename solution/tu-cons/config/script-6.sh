aa18e565c9be83add6e3ae5dfaad0928ea377e91
daveshook <alpscycler@gmail.com>
2020-05-08 21:39:03 -0400

    updated tu-cons scripts

--- solution/tu-cons/config/script-6.sh
+++ solution/tu-cons/config/script-6.sh
@@ -1,4 +1,6 @@
 export NS=io.confluent.monitoring.clients.interceptor
+yum install -y libmnl
+rpm -i --nodeps http://mirror.centos.org/centos/8/BaseOS/x86_64/os/Packages/iproute-tc-4.18.0-15.el8.x86_64.rpm
 tc qdisc add dev eth0 root netem delay 50ms
 kafka-console-consumer \
     --group group-with-6-partitions \
